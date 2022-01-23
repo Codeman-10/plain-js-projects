566cd9d0da82039b3c15a08605b1acf25b6af979
gokul <you@example.com>
2022-01-23 15:30:52 +0530

    hh

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"`)
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add("playing");
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+  key.classList.remove("playing");
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add("playing");
 }
 
 function removeTranstition(e) {
-    if (e.propertyName !== 'transform') return;
-    this.classList.remove("playing");
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) => key.classList.remove("playing"));
 }
 
-const keys = document.querySelectorAll(".key");
-console.log(keys);
-keys.forEach(key => key.addEventListener('transitionend', removeTranstition));
+function changeText(ff) {
+  document.getElementById("headertext").innerHTML = ff;
+}
 
 window.addEventListener("keydown", playSound);
+window.addEventListener("keyup", removeTranstition);
